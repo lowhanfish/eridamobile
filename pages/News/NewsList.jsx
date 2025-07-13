a2b8bedb743796b5db43001479faf2fe975b00dc
lowhanfish <lowhanfish@gmail.com>
2025-07-13 14:38:16 +0800

    added NewsList Page

---
+++ pages/News/NewsList.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+
+import { stylex } from "../assets/css";
+
+const NewsList = () => {
+    return (
+        <Text>Ini NewsList</Text>
+    )
+
+}
+
+
+
+export default NewsList
