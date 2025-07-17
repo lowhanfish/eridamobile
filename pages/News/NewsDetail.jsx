b57651abb1e9f639adbdfd2c017453b005626467
lowhanfish <lowhanfish@gmail.com>
2025-07-17 14:36:40 +0800

    Add ImageLib Lybrary

--- pages/News/NewsDetail.jsx
+++ pages/News/NewsDetail.jsx
@@ -1,29 +1,26 @@
 import { useEffect, useState, useCallback } from "react";
-import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import useGlobalStore from "../../stores/useGlobalStore";
-
 
 const NewsDetail = () => {
 
     const visibleBar = useGlobalStore((state) => state.visibleBar)
     const setRouteBack = useGlobalStore((state) => state.setRouteBack);
 
-
-
     useFocusEffect(
         useCallback(() => {
             setRouteBack("NewsList");
             visibleBar(true, true)
-
         }, [visibleBar])
     )
 
-
     return (
-        <View>
-            <Text>NewsList</Text>
+        <View style={stylex.container}>
+            <ScrollView style={stylex.scrollPage}>
+
+            </ScrollView>
         </View>
     )
 
