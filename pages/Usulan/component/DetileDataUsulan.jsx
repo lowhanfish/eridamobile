a16bbfbc4e57c321a7afb5b347bc3f28125424f6
lowhanfish <lowhanfish@gmail.com>
2025-07-31 21:59:48 +0800

    change IP Server

--- pages/Usulan/component/DetileDataUsulan.jsx
+++ pages/Usulan/component/DetileDataUsulan.jsx
@@ -2,11 +2,8 @@ import { useState, useCallback, useEffect } from "react";
 import { View, ScrollView, Text, TouchableOpacity } from "react-native";
 
 
-
-
 const DetileDataUsulan = () => {
 
 }
-
 
 export default DetileDataUsulan
