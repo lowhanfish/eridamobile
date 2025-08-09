81f2673bb3aa13046dcd6a69014e3ea080d5bc9c
lowhanfish <lowhanfish@gmail.com>
2025-08-09 19:23:49 +0800

    added react-native-webview

--- components/NewsDetail.jsx
+++ components/NewsDetail.jsx
@@ -19,7 +19,7 @@ const NewsDetail = () => {
     const urlx = useGlobalStore(state => state.url)
 
     const [cek_load_data, setCekLoadData] = useState(true);
-    const [list_data, setListData] = useState(null);
+    const [list_data, setListData] = useState([]);
 
 
 
@@ -41,6 +41,7 @@ const NewsDetail = () => {
             }
         }).then(response => {
             setCekLoadData(false)
+            setListData(response.data)
             console.log(response)
         }).catch(err => {
             setCekLoadData(false)
@@ -69,31 +70,33 @@ const NewsDetail = () => {
                         </View>
                     ) : (
 
-                        <View>
+                        list_data.map((data, i) => (
 
+                            <View key={i}>
+                                <ImageLib
+                                    urix={"https://server-erida.konaweselatankab.go.id/uploads/1752836088394.jpg"} customWidth={'100%'}
+                                    style={styles.ImgNews}
+                                />
 
-                            <ImageLib
-                                urix={"https://server-erida.konaweselatankab.go.id/uploads/1752836088394.jpg"} customWidth={'100%'}
-                                style={styles.ImgNews}
-                            />
-
-                            <Text style={styles.NewsTitle}>Kopi Tolaki Robusta Belang Wira Konawe Selatan : Dari Tradisi Lokal Menuju Panggung Dunia</Text>
-                            <View style={styles.newsAuthorContainer}>
-                                <View style={styles.newsAuthorContainerItem}>
-                                    <Image style={stylex.newsListTitleDescIcon} source={require('../pages/assets/images/icon/time.png')} />
-                                    <Text style={stylex.newsListTitleDescText}>20 March 2025</Text>
+                                <Text style={styles.NewsTitle}>{data.judul}</Text>
+                                <View style={styles.newsAuthorContainer}>
+                                    <View style={styles.newsAuthorContainerItem}>
+                                        <Image style={stylex.newsListTitleDescIcon} source={require('../pages/assets/images/icon/time.png')} />
+                                        <Text style={stylex.newsListTitleDescText}>20 March 2025</Text>
+                                    </View>
+                                    <View style={styles.newsAuthorContainerItem}>
+                                        <Image style={stylex.newsListTitleDescIcon} source={require('../pages/assets/images/icon/user.png')} />
+                                        <Text style={stylex.newsListTitleDescText}>admin_erida</Text>
+                                    </View>
                                 </View>
-                                <View style={styles.newsAuthorContainerItem}>
-                                    <Image style={stylex.newsListTitleDescIcon} source={require('../pages/assets/images/icon/user.png')} />
-                                    <Text style={stylex.newsListTitleDescText}>admin_erida</Text>
+                                <View>
+                                    <Text style={styles.newsText}>
+                                        {data.isi}
+                                    </Text>
                                 </View>
                             </View>
-                            <View>
-                                <Text style={styles.newsText}>
-                                    Sekretaris Daerah (Sekda) Kabupaten Konawe Selatan, Hj. St. Chadidjah, S.Sos., M.Si., mewakili Bupati Konawe Selatan menghadiri kegiatan penerimaan mahasiswa Kuliah Kerja Nyata Pembelajaran Pemberdayaan Masyarakat (KKN-PPM) Universitas Gadjah Mada (UGM) di Kabupaten Konawe Selatan, yang berlangsung pada Minggu, 22 Juni 2025, bertempat di Hotel Zenith Kendari. Sebanyak 30 mahasiswa dari berbagai program studi di UGM secara resmi diterima untuk menjalankan program KKN-PPM di tiga desa yang berada di wilayah Kecamatan Laonti, yakni Desa Namu, Desa Malaringgi, dan Desa Batu Jaya. Dalam sambutannya, Sekda Konawe Selatan menyampaikan apresiasi atas kepercayaan Universitas Gadjah Mada yang kembali menjadikan Konawe Selatan sebagai lokasi pengabdian. Ia berharap, kehadiran para mahasiswa dapat memberikan kontribusi positif bagi pembangunan desa, serta menjadi wadah pembelajaran langsung di tengah masyarakat.“Selamat datang kepada seluruh mahasiswa UGM di Bumi Konawe Selatan. Kami berharap KKN ini bukan hanya menjadi tugas akademik, tetapi juga pengalaman yang membentuk karakter, semangat gotong royong, dan kontribusi nyata bagi masyarakat desa,” ujar Hj. St. Chadidjah dalam sambutannya. Selama menjalankan KKN-PPM, para mahasiswa akan mengembangkan berbagai program kerja sesuai potensi lokal dan kebutuhan masyarakat desa. Kegiatan ini menjadi bagian dari implementasi tridarma perguruan tinggi, khususnya dalam hal pengabdian kepada masyarakat, serta wujud sinergi antara perguruan tinggi dan pemerintah daerah dalam mendorong pembangunan berbasis pemberdayaan masyarakat.
-                                </Text>
-                            </View>
-                        </View>
+                        ))
+
                     )}
 
 
